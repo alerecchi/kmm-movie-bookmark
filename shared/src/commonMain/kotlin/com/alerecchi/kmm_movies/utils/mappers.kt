3463fcb01b3fe028e2d95b882908ff1c688b99e0
Calogero Sanfilippo <sanfilippo.calogero@gmail.com>
2021-12-31 12:46:50 +0100

    Using ktor URLBuilder instead of java.util facilities

--- shared/src/commonMain/kotlin/com/alerecchi/kmm_movies/utils/mappers.kt
+++ shared/src/commonMain/kotlin/com/alerecchi/kmm_movies/utils/mappers.kt
@@ -1,14 +1,14 @@
 package com.alerecchi.kmm_movies.utils
 
-import java.net.URL
+import io.ktor.http.*
 
 fun String?.toImageUrl(): String {
     return if (this != null) {
-        URL(
-            "https",
-            "image.tmdb.org/t/p/original/",
-            this
-        ).toString()
+        URLBuilder(
+            protocol = URLProtocol.HTTPS,
+            host = "image.tmdb.org/t/p/original/",
+            encodedPath = this
+        ).buildString()
     } else {
         ""
     }
